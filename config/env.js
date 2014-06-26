2b56378f4860e64e22902cfd1298fbbb6c1334f2
Matt Way <matt__way@hotmail.com>
2014-06-26 11:53:27 +1000

    abstract facebook comments

--- config/env.js
+++ config/env.js
@@ -3,5 +3,8 @@
 
 module.exports = {
 	title: process.env.ST_TITLE || 'Stark Blog',
-	lastMod: new Date().toJSON().slice(0,10)
+	lastMod: new Date().toJSON().slice(0,10),
+	facebook: {
+		appId: process.env.ST_FACEBOOK_APPID
+	}
 };
