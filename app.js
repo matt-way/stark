f98c1dce8bbf0f976c6f04c441feb0a2053c4e14
Matt Way <matt__way@hotmail.com>
2016-02-10 13:14:49 +1000

    migrated to express 4

--- app.js
+++ app.js
@@ -5,7 +5,11 @@
 
 var express = require('express'),
 	http = require('http'),
-	path = require('path');
+	path = require('path'),
+	favicon = require('serve-favicon'),
+	logger = require('morgan'),
+	bodyParser = require('body-parser'),
+	errorHandler = require('errorHandler');
 
 var app = express();
 
@@ -16,20 +20,20 @@ app.set('port', process.env.PORT || 3000);
 app.set('site', path.join(__dirname, 'site'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('public', path.join(__dirname, 'public'));
-app.engine("def", require("dot-emc").init({app: app, options: { templateSettings: { cache: app.get('env') != 'development' }}}).__express);
+
+app.engine('def', require('dot-emc').init({app: app, options: { templateSettings: { cache: app.get('env') != 'development' }}}).__express);
 app.set('view engine', 'def');
-app.use(express.favicon(path.join(app.get('public'), '/images/favicon.ico')));
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
+
+app.use(favicon(path.join(app.get('public'), '/images/favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(app.get('public')));
 
 // development only
 if ('development' == app.get('env')) {
-	app.use(express.errorHandler());
+	app.use(errorHandler());
 }
 
 // initialise stark (including content routes)
